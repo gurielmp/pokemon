66e143f4f51a9fd2a49c9a3af3a397e2b7009a53
gurielamp <gurielamp@gmail.com>
2023-10-23 10:39:43 +0700

    change limit api

--- src/lib/pokemonAPI.ts
+++ src/lib/pokemonAPI.ts
@@ -2,7 +2,7 @@ const POKEMON_API = "https://pokeapi.co/api/v2/"
 
 
 export async function getPokemonList() {
-  const response = await fetch(POKEMON_API + "pokemon?limit=500&offset=0")
+  const response = await fetch(POKEMON_API + "pokemon?limit=150&offset=0")
   const data = await response.json()
   return data.results
 }
